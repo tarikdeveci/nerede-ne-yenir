a61923eda1bab99c36200dadfde4553f45dedb8f
tarık deveci <tarik.deveci@bahcesehir.edu.tr>
2025-04-29 21:24:30 +0300

    backend init

---
+++ backend/src/test/java/com/example/nerede_ne_yenir_backend/NeredeNeYenirBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nerede_ne_yenir_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeredeNeYenirBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
